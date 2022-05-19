95bae794ad6dc9d79f86c7dd51a1e1fdf25ca51e
Mateusz Kubisiak <mateusz.kubisiak@gds.ey.com>
2022-05-19 17:58:53 +0200

    Firebase init

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,14 +1,20 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 const firebaseConfig = {
-	apiKey: process.env.API_KEY,
-	authDomain: "managify-389f8.firebaseapp.com",
-	projectId: "managify-389f8",
-	storageBucket: "managify-389f8.appspot.com",
-	messagingSenderId: "838086409515",
-	appId: "1:838086409515:web:337720a4d254fcb1bf596e",
+   apiKey: process.env.API_KEY,
+   authDomain: 'managify-389f8.firebaseapp.com',
+   projectId: 'managify-389f8',
+   storageBucket: 'managify-389f8.appspot.com',
+   messagingSenderId: '838086409515',
+   appId: '1:838086409515:web:337720a4d254fcb1bf596e',
 };
 
 firebase.initializeApp(firebaseConfig);
+
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectFirestore, projectAuth, timestamp };
