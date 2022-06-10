c783dc499b057d4b106395720e58647824071921
Mateusz Kubisiak <matesz1kb@gmail.com>
2022-06-10 03:15:39 +0200

    Created useDocument hook

---
+++ src/hooks/useDocument.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import { projectFirestore } from '../firebase/config';
+
+export const useDocument = (collection, id) => {
+   const [document, setDocument] = useState(null);
+   const [error, setError] = useState(null);
+
+   //t
+   useEffect(() => {
+      const ref = projectFirestore.collection(collection).doc(id);
+
+      const unsubscribe = ref.onSnapshot(
+         (snapshot) => {
+            if (snapshot.data()) {
+               setDocument({ ...snapshot.data(), id: snapshot.id });
+               setError(null);
+            } else {
+               setError(`There is no document with ID: ${id}.`);
+            }
+         },
+         (err) => {
+            console.log(err.message);
+            setError('Failed to retrieve the document.');
+         }
+      );
+
+      return () => unsubscribe();
+   }, [collection, id]);
+
+   return { document, error };
+};
