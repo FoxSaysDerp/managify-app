d786a0629bd3b1ef2b706ba7eaa818022e0ac8bf
Mateusz <matesz1kb@gmail.com>
2022-06-14 21:36:19 +0200

    Added Task Graph component to profile

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 
 import { useDocument } from '../hooks/useDocument';
@@ -12,6 +13,7 @@ import { grey, green } from '@mui/material/colors';
 import { toast } from 'react-toastify';
 import Spinner from '../components/Spinner';
 import TaskList from '../components/TaskList';
+import TaskGraph from '../components/TaskGraph';
 
 const secondary = grey[500];
 const offline = grey[300];
@@ -30,13 +32,16 @@ const UserProfile = () => {
    const { uid } = useParams();
    const history = useHistory();
    const { document, error } = useDocument('users', uid);
-   const { documents } = useCollection('tasks', [
-      'assignedUsersIds',
-      'array-contains',
-      uid,
-   ]);
+   const { documents } = useCollection('tasks');
 
-   console.log('document', document);
+   const [taskDocs, setTaskDocs] = useState([]);
+   useEffect(() => {
+      if (documents) {
+         setTaskDocs(
+            documents.filter((task) => task.assignedUsersIds.includes(uid))
+         );
+      }
+   }, [documents]);
 
    if (error) {
       toast.error(error, {
@@ -68,124 +73,150 @@ const UserProfile = () => {
    }
 
    return (
-      <Grid container spacing={2}>
-         <Grid item xs={6}>
-            <Paper sx={{ p: 2 }}>
-               <Box>
-                  <Grid container spacing={4}>
-                     <Grid item xs={2}>
-                        <Avatar
-                           src={document.photoURL}
-                           alt={document.displayName}
-                           sx={{
-                              width: '100%',
-                              height: 'unset',
-                           }}
-                           style={{ aspectRatio: 1 }}
-                        />
-                     </Grid>
-                     <Grid
-                        item
-                        xs={10}
-                        sx={{ display: 'flex', alignItems: 'center' }}
-                     >
-                        <Stack>
-                           <Grid container spacing={3}>
-                              <Grid item>
-                                 <Typography
-                                    variant="h3"
-                                    sx={{ fontWeight: 500 }}
-                                 >
-                                    {document.displayName}
-                                 </Typography>
-                              </Grid>
-                              <Grid
-                                 item
-                                 sx={{ display: 'flex', alignItems: 'center' }}
-                              >
-                                 <AvailabilityStatus
-                                    isOnline={document.online ? 1 : undefined}
-                                 />
-                                 <Typography
-                                    variant="overline"
-                                    color={
-                                       document.online ? available : offline
-                                    }
-                                 >
-                                    {document.online ? 'online' : 'offline'}
-                                 </Typography>
-                              </Grid>
+      <Grid container spacing={2} sx={{ alignItems: 'stretch' }}>
+         <Grid item xs={8}>
+            <Grid container spacing={2}>
+               <Grid item xs={12} sx={{ height: '100%' }}>
+                  <Paper sx={{ p: 2 }}>
+                     <Box>
+                        <Grid container spacing={4}>
+                           <Grid item xs={3}>
+                              <Avatar
+                                 src={document.photoURL}
+                                 alt={document.displayName}
+                                 sx={{
+                                    width: '100%',
+                                    height: 'unset',
+                                 }}
+                                 style={{ aspectRatio: 1 }}
+                              />
                            </Grid>
-                           <Typography
-                              sx={{ display: 'block', marginTop: '5px' }}
-                              color={secondary}
-                              variant="caption"
+                           <Grid
+                              item
+                              xs={9}
+                              sx={{ display: 'flex', alignItems: 'center' }}
                            >
-                              @{document.id}
-                           </Typography>
-                        </Stack>
-                     </Grid>
-                  </Grid>
-               </Box>
-            </Paper>
-         </Grid>
-         <Grid item xs={4}>
-            <Paper sx={{ p: 2 }}>
-               <Grid container>
-                  <Grid
-                     item
-                     xs={6}
-                     sx={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        flexDirection: 'column',
-                     }}
-                  >
-                     {documents && (
-                        <Typography variant="h3" sx={{ minHeight: '48px' }}>
-                           {documents.length}
-                        </Typography>
-                     )}
-                     <Typography variant="subtitle2">tasks</Typography>
-                  </Grid>
-                  <Grid
-                     item
-                     xs={6}
-                     sx={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        flexDirection: 'column',
-                     }}
-                  >
-                     <Typography
-                        variant="h5"
-                        align="center"
-                        sx={{
-                           minHeight: '48px',
-                           display: 'flex',
-                           alignItems: 'center',
-                        }}
-                     >
-                        {/* eslint-disable */}
-                        {document.joined
-                           ? moment
-                                .unix(document?.joined?.seconds)
-                                .from(moment())
-                           : '-'}
-                        {/* eslint-enable */}
-                     </Typography>
-                     <Typography variant="subtitle2">joined</Typography>
-                  </Grid>
+                              <Stack>
+                                 <Grid container spacing={3}>
+                                    <Grid item>
+                                       <Typography
+                                          variant="h3"
+                                          sx={{ fontWeight: 500 }}
+                                       >
+                                          {document.displayName}
+                                       </Typography>
+                                    </Grid>
+                                    <Grid
+                                       item
+                                       sx={{
+                                          display: 'flex',
+                                          alignItems: 'center',
+                                       }}
+                                    >
+                                       <AvailabilityStatus
+                                          isOnline={
+                                             document.online ? 1 : undefined
+                                          }
+                                       />
+                                       <Typography
+                                          variant="overline"
+                                          color={
+                                             document.online
+                                                ? available
+                                                : offline
+                                          }
+                                       >
+                                          {document.online
+                                             ? 'online'
+                                             : 'offline'}
+                                       </Typography>
+                                    </Grid>
+                                 </Grid>
+                                 <Typography
+                                    sx={{ display: 'block', marginTop: '5px' }}
+                                    color={secondary}
+                                    variant="caption"
+                                 >
+                                    @{document.id}
+                                 </Typography>
+                              </Stack>
+                           </Grid>
+                        </Grid>
+                     </Box>
+                  </Paper>
                </Grid>
+               <Grid item xs={12} style={{ height: '100%' }}>
+                  <Paper sx={{ p: 2 }}>
+                     <Grid container>
+                        <Grid
+                           item
+                           xs={6}
+                           sx={{
+                              display: 'flex',
+                              justifyContent: 'center',
+                              alignItems: 'center',
+                              flexDirection: 'column',
+                           }}
+                        >
+                           <Typography
+                              variant="h5"
+                              align="center"
+                              sx={{
+                                 minHeight: '48px',
+                                 display: 'flex',
+                                 alignItems: 'center',
+                              }}
+                           >
+                              {/* eslint-disable */}
+                              {document.joined
+                                 ? moment
+                                      .unix(document?.joined?.seconds)
+                                      .from(moment())
+                                 : '-'}
+                              {/* eslint-enable */}
+                           </Typography>
+                           <Typography variant="subtitle2">joined</Typography>
+                        </Grid>
+                        <Grid
+                           item
+                           xs={6}
+                           sx={{
+                              display: 'flex',
+                              justifyContent: 'center',
+                              alignItems: 'center',
+                              flexDirection: 'column',
+                           }}
+                        >
+                           {taskDocs && (
+                              <Typography
+                                 variant="h3"
+                                 sx={{ minHeight: '48px' }}
+                              >
+                                 {taskDocs.length}
+                              </Typography>
+                           )}
+                           <Typography variant="subtitle2">
+                              tasks assigned
+                           </Typography>
+                        </Grid>
+                     </Grid>
+                  </Paper>
+               </Grid>
+            </Grid>
+         </Grid>
+         <Grid item xs={3}>
+            <Paper sx={{ p: 2 }} style={{ height: '100%' }}>
+               <Typography align="center" variant="subtitle2">
+                  Task assignment
+               </Typography>
+               <TaskGraph data={taskDocs} />
             </Paper>
          </Grid>
          <Grid item xs={12}>
             <Paper sx={{ p: 2 }}>
                <TaskList
                   label={`${document.displayName}'s tasks`}
-                  tasks={documents}
+                  tasks={taskDocs}
                />
             </Paper>
          </Grid>
