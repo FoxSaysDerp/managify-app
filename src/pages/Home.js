7ae2c45a5be2243bee1cc0a8856a3df5d970bf0b
Mateusz Kubisiak <matesz1kb@gmail.com>
2022-06-22 13:27:58 +0200

    Added task list to home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import { Grid, Paper, Stack } from '@mui/material';
+import { useState, useEffect } from 'react';
+import { useAuthContext } from '../hooks/useAuthContext';
+import { useCollection } from '../hooks/useCollection';
+import { Grid, Paper, Stack, Typography } from '@mui/material';
 
 import UserList from '../components/UserList';
+import TaskList from '../components/TaskList';
 
 const Home = () => {
+   const { user } = useAuthContext();
+
+   const { documents } = useCollection('tasks');
+
+   const [taskDocs, setTaskDocs] = useState([]);
+   useEffect(() => {
+      if (documents) {
+         setTaskDocs(
+            documents.filter((task) => task.assignedUsersIds.includes(user.uid))
+         );
+      }
+   }, [documents, user]);
    return (
       <>
          <Grid container spacing={3}>
@@ -14,19 +29,12 @@ const Home = () => {
                         p: 2,
                         display: 'flex',
                         flexDirection: 'column',
-                        height: 240,
                      }}
                   >
-                     <span>Test</span>
-                  </Paper>
-                  <Paper
-                     sx={{
-                        p: 2,
-                        display: 'flex',
-                        flexDirection: 'column',
-                     }}
-                  >
-                     <span>Test</span>
+                     <Typography variant="h5" sx={{ mb: 4 }}>
+                        Hello, <strong>{user.displayName}</strong>
+                     </Typography>
+                     <TaskList label="Your tasks" tasks={taskDocs} />
                   </Paper>
                </Stack>
             </Grid>
